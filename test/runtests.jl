50632a349a9f071493f75dfb1fb0b5a2c2e47a7c
Karl Wessel <karl.wessel@stud.uni-goettingen.de>
2018-10-08 15:20:00 +0200

    added at least one plot test

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,5 +1,11 @@
 using UnitfulPlots
-using Base.Test
+using Test
+using Plots
+using Unitful.DefaultSymbols
 
 # write your own tests here
-@test 1 == 2
+@testset "At least create one plot" begin
+    data = rand(10)*m
+
+    @test isa(plot(data), Plots.Plot) == true
+end
